2c1f437beee0b0d4a4eeaac0a95c2b898a76f55e
mthology <matt@attribyte.com>
2014-11-06 01:48:25 -0600

    Add license

--- src/main/java/org/attribyte/api/http/impl/commons/Commons4Client.java
+++ src/main/java/org/attribyte/api/http/impl/commons/Commons4Client.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2010 Attribyte, LLC 
+ * Copyright 2014 Attribyte, LLC
  * 
  * Licensed under the Apache License, Version 2.0 (the "License"); 
  * you may not use this file except in compliance with the License. 
