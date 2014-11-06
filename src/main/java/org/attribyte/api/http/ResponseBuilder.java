bde91aeb0f34c6f80a08eaf2eb14eaccabb1352a
mthology <matt@attribyte.com>
2014-11-05 18:22:37 -0600

    Build complete/streaming responses. Change method name to putHeaders()

--- src/main/java/org/attribyte/api/http/ResponseBuilder.java
+++ src/main/java/org/attribyte/api/http/ResponseBuilder.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2010 Attribyte, LLC 
+ * Copyright 2010,2014 Attribyte, LLC
  * 
  * Licensed under the Apache License, Version 2.0 (the "License"); 
  * you may not use this file except in compliance with the License. 
@@ -15,14 +15,17 @@
 
 package org.attribyte.api.http;
 
+import com.google.common.base.Charsets;
 import com.google.common.collect.Maps;
+import com.google.common.io.ByteSource;
+import com.google.common.io.ByteStreams;
+import com.google.protobuf.ByteString;
 
-import java.io.UnsupportedEncodingException;
 import java.util.Collection;
 import java.util.Map;
 
 /**
- * Builds HTTP responses.
+ * Builds immutable HTTP responses.
  */
 public class ResponseBuilder {
 
@@ -41,31 +44,37 @@ public class ResponseBuilder {
    }
 
    /**
-    * Creates a response builder with a response code and body.
+    * Creates a response builder with a response code and body as bytes.
     * @param statusCode The response code.
     * @param body The response body.
     */
    public ResponseBuilder(final int statusCode, final byte[] body) {
       this.statusCode = statusCode;
-      this.body = body;
+      this.body = ByteString.copyFrom(body);
    }
 
    /**
-    * Create a response builder with a response code and body.
+    * Creates a response builder with a response code and body as a string.
     * @param statusCode The response code.
     * @param body The response body.
     */
    public ResponseBuilder(final int statusCode, final String body) {
       this.statusCode = statusCode;
-      try {
-         this.body = body.getBytes("UTF-8");
-      } catch(UnsupportedEncodingException uee) {
-         this.body = body.getBytes();
-      }
+      this.body = ByteString.copyFrom(body.getBytes(Charsets.UTF_8));
    }
 
    /**
-    * Sets the response code for the response.
+    * Creates a response builder with a response code and body as a source.
+    * @param statusCode The response code.
+    * @param bodySource The response body source.
+    */
+   public ResponseBuilder(final int statusCode, final ByteSource bodySource) {
+      this.statusCode = statusCode;
+      this.bodySource = bodySource;
+   }
+
+   /**
+    * Sets the response code.
     * @param statusCode The response code.
     * @return A self-reference.
     */
@@ -75,44 +84,59 @@ public class ResponseBuilder {
    }
 
    /**
-    * Sets the response body.
+    * Sets the response body bytes.
     * @param body The body.
     * @return A self-reference.
     */
-   public ResponseBuilder setBody(byte[] body) {
-      this.body = body;
+   public ResponseBuilder setBody(final byte[] body) {
+      this.body = ByteString.copyFrom(body);
       return this;
    }
 
    /**
-    * Sets the response body.
+    * Sets the response body as a string.
     * <p>
     * Converts to <code>UTF-8</code>
     * </p>
     * @param body The body.
     * @return A self-reference.
     */
-   public ResponseBuilder setBody(String body) {
-
-      try {
-         this.body = body.getBytes("UTF-8");
-      } catch(UnsupportedEncodingException uee) {
-         this.body = body.getBytes();
-      }
-
+   public ResponseBuilder setBody(final String body) {
+      this.body = ByteString.copyFrom(body.getBytes(Charsets.UTF_8));
       return this;
    }
 
    /**
-    * Creates the response.
-    * @return The response.
+    * Sets a byte source for the response body.
+    * @param body The body byte source.
+    * @return A self-reference.
     */
-   public Response create() {
-      return new Response(statusCode, headers, body);
+   public ResponseBuilder setBody(ByteSource body) {
+      this.bodySource = body;
+      return this;
    }
 
    /**
-    * Adds a header for the request to be built.
+    * Creates the immutable response.
+    * @return The response.
+    */
+   public Response create() {
+      return bodySource == null ? new CompletedResponse(statusCode, headers, body, attributes) :
+              new StreamedResponse(statusCode, headers, bodySource, attributes);
+   }
+
+   /**
+    * Creates an immutable streamed response.
+    * @return The response.
+    */
+   public StreamedResponse createStreamed() {
+      final ByteSource bodySource =
+              this.bodySource != null ? this.bodySource : body != null ? ByteStreams.asByteSource(body.toByteArray()) : null;
+      return new StreamedResponse(statusCode, headers, bodySource, attributes);
+   }
+
+   /**
+    * Adds a header.
     * @param name The header name.
     * @param value The header value.
     * @return A self-reference.
@@ -129,21 +153,21 @@ public class ResponseBuilder {
    }
 
    /**
-    * Adds a collection of headers for the request to be built.
+    * Adds a collection of headers.
     * @param headers The headers to add.
     * @return A self-reference.
     */
-   public ResponseBuilder addHeaders(final Collection<Header> headers) {
+   public ResponseBuilder putHeaders(final Collection<Header> headers) {
       if(headers != null) {
          for(Header header : headers) {
-            this.headers.put(header.getName(), header);
+            this.headers.put(header.getName().toLowerCase(), header);
          }
       }
       return this;
    }
 
    /**
-    * Adds a map of headers for the request.
+    * Adds a map of headers.
     * @param headerMap The map of headers.
     * @return A self-reference.
     */
@@ -155,34 +179,45 @@ public class ResponseBuilder {
    }
 
    /**
-    * Adds an attribute to the request to be built.
+    * Adds an attribute.
     * @param name The attribute name.
     * @param object The attribute value.
     * @return A self-reference.
     */
    public ResponseBuilder addAttribute(final String name, final Object object) {
-      if(this.attributes == null) {
-         this.attributes = Maps.newHashMapWithExpectedSize(4);
-      }
-      this.attributes.put(name, object);
+      maybeCreateAttributes().put(name, object);
       return this;
    }
 
    /**
-    * Adds a map of attributes for the request.
+    * Adds a map of attributes.
     * @param attributes The map of attributes.
     * @return A self-reference.
     */
    public ResponseBuilder addAttributes(final Map<String, Object> attributes) {
       if(attributes != null) {
-         this.attributes.putAll(attributes);
+         maybeCreateAttributes().putAll(attributes);
       }
       return this;
    }
 
-   final Map<String, Header> headers = Maps.newHashMapWithExpectedSize(16);
+   /**
+    * Creates the attributes map if necessary.
+    * @return The attributes.
+    */
+   private Map<String, Object> maybeCreateAttributes() {
+      if(this.attributes == null) {
+         this.attributes = Maps.newHashMapWithExpectedSize(4);
+      }
+      return this.attributes;
+   }
+
+   //Every response is going to have headers...
+   final Map<String, Header> headers = Maps.newHashMapWithExpectedSize(8);
+   //...but maybe will have attributes...
    Map<String, Object> attributes = null;
-   byte[] body;
+   ByteString body = null;
+   ByteSource bodySource = null;
+   //Constructors force this to be initialized to something...
    int statusCode;
 }
-
