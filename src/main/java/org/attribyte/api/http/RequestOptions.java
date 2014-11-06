caeab42b1b27851e71249a7f1150917651261ce0
mthology <matt@attribyte.com>
2014-11-05 18:21:33 -0600

    Add license. Document default timeout seconds.

--- src/main/java/org/attribyte/api/http/RequestOptions.java
+++ src/main/java/org/attribyte/api/http/RequestOptions.java
@@ -1,7 +1,22 @@
+/*
+ * Copyright 2014 Attribyte, LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and limitations under the License.
+ *
+ */
+
 package org.attribyte.api.http;
 
 /**
- * Encapsulates all request options.
+ * Request-specific options.
  */
 public class RequestOptions {
 
@@ -15,10 +30,17 @@ public class RequestOptions {
     */
    public static final int DEFAULT_MAX_RESPONSE_BYTES = 1024 * 1024;
 
+   /**
+    * The maximum time to wait for a response (5 seconds).
+    */
+   public static final int DEFAULT_TIMEOUT_SECONDS = 5;
+
+   /**
+    * The default request options.
+    */
    public static final RequestOptions DEFAULT = new RequestOptions(
            DEFAULT_FOLLOW_REDIRECTS,
-           DEFAULT_MAX_RESPONSE_BYTES, 5);
-
+           DEFAULT_MAX_RESPONSE_BYTES, DEFAULT_TIMEOUT_SECONDS);
 
    /**
     * Creates request options.
