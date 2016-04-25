25395c36bf40ed082d6ba1fe09792e105d1019f7
mthology <matt@attribyte.com>
2016-04-25 09:49:33 -0500

    Use multi-catch Don't lose interrupt status

--- src/main/java/org/attribyte/api/http/impl/jetty/JettyClient.java
+++ src/main/java/org/attribyte/api/http/impl/jetty/JettyClient.java
@@ -115,11 +115,10 @@ public class JettyClient implements AsyncClient {
    public org.attribyte.api.http.Response send(org.attribyte.api.http.Request request, RequestOptions options) throws IOException {
       try {
          return asyncSend(request, options).get(options.timeoutSeconds, TimeUnit.SECONDS);
-      } catch(TimeoutException te) {
-         throw new IOException(te);
-      } catch(ExecutionException ee) {
-         throw new IOException(ee);
+      } catch(TimeoutException | ExecutionException e) {
+         throw new IOException(e);
       } catch(InterruptedException ie) {
+         Thread.currentThread().interrupt();
          throw new IOException(ie);
       }
    }
